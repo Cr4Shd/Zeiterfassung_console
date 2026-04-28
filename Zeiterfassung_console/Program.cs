c0b5460ba797dbd7835c00d48459e348f9a589b6
Cr4ShD <niemeyer.9216@gmail.com>
2026-04-28 08:32:36 +0200

    Projektdateien hinzufügen.

---
+++ Zeiterfassung_console/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+
+namespace Zeiterfassung_console
+{
+    class Program
+    {
+        public enum MenuItemsSum
+        {
+            Kommen = 0,
+            Gehen = 1,
+            Drucke_Zeiten = 2
+        }
+        public MenuItemsSum MenuItems { get; set; }
+        static void Main(string[] args)
+        {
+            Console.Title = "Zeiterfassung";
+            Console.WriteLine("Willkommen zur Zeiterfassung!\n");
+            Console.WriteLine("Was möchten Sie tun?");
+            PrintMenuItems();
+            Console.WriteLine("Bitte wählen Sie aus...");
+            var x = Console.Read();
+            
+        }
+
+        /// <summary>
+        /// Druckt das Menü zum Start
+        /// </summary>
+        public static void PrintMenuItems()
+        {
+            string[] items = new string[3];
+            for (int i = 0; i < items.Length; i++)
+            {
+                
+                Console.WriteLine($"{i+1}: {(MenuItemsSum)i}");
+            }
+        }
+        /// <summary>
+        /// Erfasst Start und Endzeit und bereitet alles Vor zum Speichern vor. Zum Speichern wird ein Objekt genutzt!
+        /// </summary>
+        public ZeitObjekt SpeicherVorbereitungAlsObjekt()
+        {
+            DateTime nw = DateTime.Now;
+            Console.WriteLine($"Heute ist der {nw}");
+            Console.WriteLine("Wann wurde angefangen?");
+            string? start = Console.ReadLine();
+            Console.WriteLine("Wann ist Ende?");
+            string? end = Console.ReadLine();
+            ZeitObjekt ze = new(start, end);
+            return ze;
+        }
+        /// <summary>
+        /// Erfasst Start und Endzeit und bereitet alles Vor zum Speichern vor.
+        /// </summary>
+        public void SpeichereZeitenAlsTextDatei()
+        {
+            DateTime nw = DateTime.Now;
+            Console.WriteLine($"Heute ist der {nw}");
+            Console.WriteLine("Wann wurde angefangen?");
+            string? start = Console.ReadLine();
+            Console.WriteLine("Wann ist Ende?");
+            string? end = Console.ReadLine();
+
+            //Erstelle dir eine Liste von string (als Array)
+            string[] lines = {$"Beginn:{start}", $"Ende:{end}", $"Datum:{nw}"};
+
+            // Kläre den Pfad, auf welchem du später Speichern möchtest
+            string docPath =
+              Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Schreibe eine Datei in deinen DokumentenOrdner
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "ZeitenVonNico.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+
+        }
+
+        
+        public void SchreibeZeitenInDateiObjekt()
+        {
+
+        }
+
+        public void PrintTime()
+        {
+
+        }
+    }
+}
---
+++ Zeiterfassung_console/ZeitObjekt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeiterfassung_console
+{
+    class ZeitObjekt
+    {
+        public DateTime CreationTime { get; set; } 
+        public string Start { get; set; }
+        public string End { get; set; }
+
+        public ZeitObjekt(string sta, string end)
+        {
+            this.Start = sta;
+            this.End = end;
+            CreationTime = DateTime.Now;
+
+        }
+    }
+}
